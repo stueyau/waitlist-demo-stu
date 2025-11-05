b0cd55a6458507e9acb44501e05ed32a8bc4260f
stueyau <stuziman@gmail.com>
2025-11-06 09:29:11 +1000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: '/api/farcaster',
+      },
+    ];
   },
 };
-
-export default nextConfig;
